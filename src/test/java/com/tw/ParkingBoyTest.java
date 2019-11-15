8a6ce8ecc665514dce04412843bcf6b3b8ad3561
jiaheng-zhang <jiaheng.zhang@rea-group.com>
2019-11-15 18:46:01 +0800

    提交：泊车小弟第一个 test case 修改

--- src/test/java/com/tw/ParkingBoyTest.java
+++ src/test/java/com/tw/ParkingBoyTest.java
@@ -2,7 +2,11 @@ package com.tw;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * unit test for Parking boy
@@ -11,11 +15,14 @@ public class ParkingBoyTest {
 
     @Test
     void givenAParkingBoyOneCarportParkingLotOneCarWhenParkingThenGetATicket(){
-        ParkingBoy parkingBoy = new ParkingBoy();
         int id = 1;
         ParkingLot parkingLot = new ParkingLot(1, id);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
         Car car = new Car("陕 A88888");
         Ticket ticket = parkingBoy.park(car);
+        assertNotNull(ticket);
         assertEquals(ticket.getCarNumber(), car.getCarNumber());
         assertEquals(ticket.getParkingLotId(), id);
     }
