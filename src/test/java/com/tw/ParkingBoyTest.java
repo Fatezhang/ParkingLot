5bea7b57dbf6aea8261b4f3c7c1649d298c8fa66
jiaheng-zhang <jiaheng.zhang@rea-group.com>
2019-11-16 12:48:21 +0800

    提交：泊车小弟 test case ：givenAParkingBoyTwoEmptyParkingLotsWithTwoCarportsWhenParkingTwoCarsThenGetTicketsBindsParkingLots

--- src/test/java/com/tw/ParkingBoyTest.java
+++ src/test/java/com/tw/ParkingBoyTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertNotNull;
 public class ParkingBoyTest {
 
     @Test
-    public void givenAParkingBoyOneCarportParkingLotOneCarWhenParkingThenGetATicket() throws Exception{
+    public void givenAParkingBoyOneCarportParkingLotOneCarWhenParkingThenGetATicket() {
         int id = 1;
         ParkingLot parkingLot = new ParkingLot(1, id);
         List<ParkingLot> parkingLots = new ArrayList<>();
@@ -28,7 +28,7 @@ public class ParkingBoyTest {
     }
 
     @Test
-    public void givenParkingLotWithACarAndATicketWhenPickCarThenGetThisCar() throws Exception{
+    public void givenAParkingBoyParkingLotWithACarAndATicketWhenPickCarThenGetThisCar() {
         int id = 1;
         String carNumber = "陕 A88888";
         ParkingLot parkingLot = new ParkingLot(1, id);
@@ -37,11 +37,34 @@ public class ParkingBoyTest {
         parkingLots.add(parkingLot);
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
         parkingBoy.park(car);
-        Ticket ticket = new Ticket(carNumber,parkingLot.getId());
+        Ticket ticket = new Ticket(carNumber, parkingLot.getId());
         Car newCar = parkingBoy.pick(ticket);
         assertEquals(ticket.getCarNumber(), newCar.getCarNumber());
     }
 
+    @Test
+    public void givenAParkingBoyTwoEmptyParkingLotsWithTwoCarportsWhenParkingTwoCarsThenGetTicketsBindsParkingLots() {
+        int parkingLotAId = 1;
+        int parkingLotBId = 2;
+        String carANumber = "陕 A88888";
+        String carBNumber = "陕 A66666";
+        Car carA = new Car(carANumber);
+        Car carB = new Car(carBNumber);
+        ParkingLot parkingLotA = new ParkingLot(2, parkingLotAId);
+        ParkingLot parkingLotB = new ParkingLot(2, parkingLotBId);
+        List<ParkingLot> lotList = new ArrayList<>();
+        lotList.add(parkingLotA);
+        lotList.add(parkingLotB);
+        ParkingBoy parkingBoy = new ParkingBoy(lotList);
+
+        Ticket ticketA = parkingBoy.park(carA);
+        Ticket ticketB = parkingBoy.park(carB);
+
+        assertEquals(ticketA.getCarNumber(), carANumber);
+        assertEquals(ticketB.getCarNumber(), carBNumber);
+        assertEquals(ticketA.getParkingLotId(), parkingLotAId);
+        assertEquals(ticketB.getParkingLotId(), parkingLotBId);
+    }
 
 
 }
